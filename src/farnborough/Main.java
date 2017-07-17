ea595bdaefcfacdaae6b595fa4738420edbb06aa
Steve Birtles <sbirtles@farnborough.ac.uk>
2017-07-17 11:21:15 +0100

    Tweak

--- src/farnborough/Main.java
+++ src/farnborough/Main.java
@@ -3,6 +3,7 @@ package farnborough;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello cloud!");
+	    System.out.println("Hello (funky) cloud!");
+
     }
 }
